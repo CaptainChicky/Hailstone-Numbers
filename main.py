5e97254760dab4b4ba5945941a851e2c397713d9
Captain Chicky <maxfang2015@yahoo.com>
2022-08-05 08:13:50 +0000

    fnished project

--- main.py
+++ main.py
@@ -1,47 +1,64 @@
-import matplotlib as mpl 
+import matplotlib as mpl
 mpl.use('Agg')
-import matplotlib.pyplot as plt 
-n = 6171
+import matplotlib.pyplot as plt
+
 plt.xlabel('Number of iterations')
 plt.ylabel('Number value')
-plt.title('Hailstone numbers: number %i' % n)
+plt.title('Hailstone numbers comparison')
 
 def get_cmap(n, name='hsv'):
-    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
-    RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
 
-count = 0
-Arr = [[0],[n]]
+maxlist = [[], []]
 
-while n > 1:
-    print(n)
-    if n % 2 == 0:
-        n = n / 2
-    else:
-        n = ((n * 3) + 1)
-    count += 1
-    Arr[0].insert(count,count)
-    Arr[1].insert(count,n)
-    
-print(n)
-x = Arr[0]
-y = Arr[1]
-plt.plot(x, y, 'k,-', linewidth=0.9)
+plotlist = [
+  163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 27
+]
 
-cmap = get_cmap(len(x))
+PLlength = len(plotlist)
 
-for k in range(count + 1):
-  plt.plot(x[k],y[k],color=cmap(k), marker='.', markersize=1.9)
+def plot(n, color):
+    count = 0
+    Arr = [[0], [n]]
+    while n > 1:
+        if n % 2 == 0:
+            n = n / 2
+        else:
+            n = ((n * 3) + 1)
+        count += 1
+        Arr[0].insert(count, count)
+        Arr[1].insert(count, n)
 
-plt.xlim(left=-0.0053 * max(x))
-plt.xlim(right= 1.01 * max(x))
-plt.ylim(bottom=-0.0053 * max(y))
-plt.ylim(top=1.02 * max(y))
+    x = Arr[0]
+    y = Arr[1]
 
-txt="Made by Chicky"
+    plt.plot(x, y, ',-', linewidth=0.9, color=color)
+
+    maxlist[0].append(max(x))
+    maxlist[1].append(max(y))
+
+def plotall(colorparse):
+    cmap = get_cmap(colorparse + 1)
+  
+    for k in range(PLlength):
+        plot(plotlist[k], cmap(k + 1))
+
+    plt.xlim(left=-0.0053 * max(maxlist[0]))
+    plt.xlim(right=1.01 * max(maxlist[0]))
+    plt.ylim(bottom=-0.0053 * max(maxlist[1]))
+    plt.ylim(top=1.02 * max(maxlist[1]))
+
+plotall(PLlength)
+
+txt = "Made by Chicky"
 #first num is percent on x axis, second is y
-plt.figtext(0.05, 0.03, txt, wrap=True, horizontalalignment='center', fontsize=8, color="grey")
+plt.figtext(0.05,
+            0.03,
+            txt,
+            wrap=True,
+            horizontalalignment='center',
+            fontsize=8,
+            color="grey")
 
 plt.tight_layout()
 
@@ -49,4 +66,3 @@ dpi = 500
 plt.subplots_adjust(left=0.15, bottom=0.1)
 
 plt.savefig('graph.png', dpi=dpi)
-#plot multiple numbers on one graph later
---
+++ origina.main.py
@@ -0,0 +1,51 @@
+import matplotlib as mpl 
+mpl.use('Agg')
+import matplotlib.pyplot as plt 
+n = 6171
+plt.xlabel('Number of iterations')
+plt.ylabel('Number value')
+plt.title('Hailstone numbers: number %i' % n)
+
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
+count = 0
+Arr = [[0],[n]]
+
+while n > 1:
+    print(n)
+    if n % 2 == 0:
+        n = n / 2
+    else:
+        n = ((n * 3) + 1)
+    count += 1
+    Arr[0].insert(count,count)
+    Arr[1].insert(count,n)
+    
+print(n)
+x = Arr[0]
+y = Arr[1]
+plt.plot(x, y, 'k,-', linewidth=0.9)
+
+cmap = get_cmap(len(x))
+
+for k in range(count + 1):
+  plt.plot(x[k],y[k],color=cmap(k), marker='.', markersize=1.9)
+
+plt.xlim(left=-0.0053 * max(x))
+plt.xlim(right= 1.01 * max(x))
+plt.ylim(bottom=-0.0053 * max(y))
+plt.ylim(top=1.02 * max(y))
+
+txt="Made by Chicky"
+#first num is percent on x axis, second is y
+plt.figtext(0.05, 0.03, txt, wrap=True, horizontalalignment='center', fontsize=8, color="grey")
+
+plt.tight_layout()
+
+dpi = 500
+plt.subplots_adjust(left=0.15, bottom=0.1)
+
+plt.savefig('graph.png', dpi=dpi)
